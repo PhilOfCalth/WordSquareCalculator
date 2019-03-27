4063a2cfe666b13064d84294ca52e1c216316b5a
PhilOfCalth <philiplarkin8383@gmail.com>
2019-03-27 20:14:13 +0000

    initial commit

---
+++ src/main/java/naimuri/larkinp/WordSquareCalculator/App.java
@@ -0,0 +1,13 @@
+package naimuri.larkinp.WordSquareCalculator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
